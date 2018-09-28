5a111810c145a61035d4a7eabff5a178ffeef557
xiuming.wang <xiuming.wang>
2018-09-28 09:15:53 +0800

    shuyun-ui 优化 角色删除出现弹框提示

--- src/views/admin/role/index.vue
+++ src/views/admin/role/index.vue
@@ -273,17 +273,24 @@ export default {
       this.form.roleDeptId = data.id;
       this.form.deptName = data.name;
     },
+    //删除角色
     handleDelete(row) {
-      delObj(row.roleId).then(response => {
-        this.dialogFormVisible = false;
-        this.getList();
-        this.$notify({
-          title: "成功",
-          message: "删除成功",
-          type: "success",
-          duration: 2000
+        this.$confirm("此操作将永久删除该角色(角色名:" + row.roleName + "), 是否继续?","提示",{
+            confirmButtonText: "确定",
+            cancelButtonText: "取消",
+            type: "warning"
+        }).then(() => {
+            delObj(row.roleId).then(response => {
+                this.dialogFormVisible = false;
+                this.getList();
+                this.$notify({
+                    title: "成功",
+                    message: "删除成功",
+                    type: "success",
+                    duration: 2000
+                });
+            }).cache(() => {});
         });
-      });
     },
     create(formName) {
       const set = this.$refs;
